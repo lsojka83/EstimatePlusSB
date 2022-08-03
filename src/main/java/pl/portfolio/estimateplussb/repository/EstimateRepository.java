9d549239bace7eba6757a010ddda635bffa0d0c0
lsojka83 <lukas_s@vp.pl>
2022-08-03 21:56:24 +0200

    Update repositores to heroku schema

--- src/main/java/pl/portfolio/estimateplussb/repository/EstimateRepository.java
+++ src/main/java/pl/portfolio/estimateplussb/repository/EstimateRepository.java
@@ -6,9 +6,13 @@ import pl.portfolio.estimateplussb.entity.Estimate;
 
 public interface EstimateRepository extends JpaRepository<Estimate, Long> {
 
-    @Query(value = "SELECT * FROM estimateplussb.estimate e " +
-            "JOIN  estimateplussb.user_estimate ue ON e.id = ue.estimates_id " +
-            "JOIN estimateplussb.user u ON ue.User_id = u.id " +
+//    @Query(value = "SELECT * FROM estimateplussb.estimate e " +
+//            "JOIN  estimateplussb.user_estimate ue ON e.id = ue.estimates_id " +
+//            "JOIN estimateplussb.user u ON ue.User_id = u.id " +
+//            "WHERE e.name = ?1 AND u.user_name = ?2",nativeQuery = true)
+    @Query(value = "SELECT * FROM heroku_266825118c34932.estimate e " +
+            "JOIN  heroku_266825118c34932.user_estimate ue ON e.id = ue.estimates_id " +
+            "JOIN heroku_266825118c34932.user u ON ue.User_id = u.id " +
             "WHERE e.name = ?1 AND u.user_name = ?2",nativeQuery = true)
     Estimate findByNameAndUserName(String estimateName, String userName);
 }
